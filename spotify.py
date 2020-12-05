d9c03018c765753603af9f197e16a8e6521d6389
NickBenevento <nbenevento99@gmail.com>
2020-12-05 17:00:13 -0500

    [unfinished] one-hot encoding artists field
    - was able to make separate dataframe with the one-hot encoded artists;
    need to add these columns back in to the original

--- spotify.py
+++ spotify.py
@@ -1,23 +1,84 @@
+import ast
 import pandas
 import numpy as np
+import re
 
 from pandas import DataFrame
+from sklearn.preprocessing import MinMaxScaler, MultiLabelBinarizer, OneHotEncoder
+from typing import List
 
 
 def parse_file(file_name: str) -> DataFrame:
     data: DataFrame = pandas.read_csv(file_name, header=0)
 
-    # Fields to drop: year, artists, id?, name?, release date?
-    # Fields to one-hot encode: key, year/release_date?
-    # Fields to scale: durability_ms, loudness?, tempo, year?
-    # I guess try to predict the artist? year/release date would probably be important then
+    # Can uncomment this line if we want to only use a subset of the rows
+    data = data.iloc[0:10]
+
+    artists_df = DataFrame(columns=['artists'])
+    for i, line in enumerate(data['artists']):
+        # convert the string to a list
+        # line = re.sub('\'', '', line)
+        # artists_df = artists_df.append({'artists': line}, ignore_index=True)
+        converted = ast.literal_eval(line)
+        artists_df = artists_df.append({'artists': converted}, ignore_index=True)
+
+        # artists = list()
+        # for artist in converted:
+        #     artists.append(artist)
+        #     if artist not in artists:
+        #         artists.append(artist)
+        # artists = ''.join(artists)
+        # print(artists)
+        # artists_df = artists_df.append({'artists': artists}, ignore_index=True)
+    print(artists_df)
+    # data = data['artists'].str.join('|').str.get_dummies()
+    mlb = MultiLabelBinarizer()
+    test = DataFrame(mlb.fit_transform(artists_df['artists']), columns=mlb.classes_, index=artists_df.index)
+    # test = artists_df['artists'].str.join('').str.get_dummies()
+
+    print(test)
+    # OneHotEncode the 'artists' field
+    # enc = OneHotEncoder()
+    # artists: List[str] = list()
+
+    # # artists = artists.values.reshape(-1, 1)
+    # enc.fit(artists_df)
+    # print(enc.categories_)
+    # data.drop('artists', axis=1, inplace=True)
+
     return data
+
+
+def predict_popularity(data: DataFrame) -> None:
+    # Drop the columns that aren't relevant
+    # data.drop(['id', 'release_date', 'artists'], axis=1, inplace=True)
+    data.drop(['id', 'release_date'], axis=1, inplace=True)
+
+    # Save the identifiers/labels to add back in after
+    names = data['name']
+    labels = data['popularity']
+    data.drop(['name', 'popularity'], axis=1, inplace=True)
+
+    # Scale the data
+    scaler = MinMaxScaler()
+    scaled = DataFrame(scaler.fit_transform(data.iloc[:, data.columns != 'name']), columns=data.columns)
+
+    # Add the identifiers/labels back to the dataframe
+    scaled['name'] = names
+    scaled['popularity'] = labels
+    print(scaled)
+
+    # The 'name' field should not be used for the model, but I'm keeping it
+    #   for printing purposes
+
+    # todo: implement model
+    return
 
 
 def main():
     data = parse_file('data.csv')
-    print(data)
+    # predict_popularity(data)
     return
 
-main()
 
+main()
