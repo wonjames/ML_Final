fa97ae7b86c83a7bbd62721c5686c4e8f86df467
Alex Broaddus <alexbroaddus1@gmail.com>
2020-12-07 17:54:10 -0500

    Added scatter plots against popularity

--- data_visualization.py
+++ data_visualization.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 
-spotify_df = pandas.read_csv('data.csv', header=0, nrows=10000)
-print(spotify_df['valence'])
+spotify_df = pandas.read_csv('data.csv', header=0)
 
 # Initialize 16 histogram plots for numeric features
 fig, axs = plt.subplots(4, 4, figsize=(12, 10), tight_layout=True)
+fig.delaxes(axs[3][3])
 num_bins = 20
 
 # Add each feature to histogram plot
@@ -19,8 +19,8 @@ axs[0][1].set_title('Acousticness')
 axs[0][2].hist(spotify_df['danceability'], bins=num_bins)
 axs[0][2].set_title('Danceability')
 
-axs[0][3].hist(spotify_df['duration_ms'], bins=num_bins)
-axs[0][3].set_title('Duration (ms)')
+axs[0][3].hist(spotify_df['duration_ms'] / 1000, bins=num_bins)
+axs[0][3].set_title('Duration (seconds)')
 
 axs[1][0].hist(spotify_df['energy'], bins=num_bins)
 axs[1][0].set_title('Energy')
@@ -55,9 +55,30 @@ axs[3][0].hist(spotify_df['speechiness'], bins=num_bins)
 axs[3][0].set_title('Speechiness')
 
 axs[3][1].hist(spotify_df['tempo'], bins=num_bins)
-axs[3][1].set_title('Tempo')
+axs[3][1].set_title('Tempo (BPM)')
 
 axs[3][2].hist(spotify_df['year'], bins=num_bins)
 axs[3][2].set_title('Release Year')
+
+plt.show()
+
+# Initialize a 4x4 plots for plotting against popularity
+fig, axs = plt.subplots(2, 2, figsize=(12, 10), tight_layout=True)
+
+axs[0][0].scatter(spotify_df['speechiness'], spotify_df['popularity'], color='#cc4365', s=5)
+axs[0][0].set_xlabel('Speechiness')
+axs[0][0].set_ylabel('Popularity')
+
+axs[0][1].scatter(spotify_df['danceability'], spotify_df['popularity'], color='#cc4365', s=5)
+axs[0][1].set_xlabel('Danceability')
+axs[0][1].set_ylabel('Popularity')
+
+axs[1][0].scatter(spotify_df['duration_ms'] / 1000, spotify_df['popularity'], color='#cc4365', s=5)
+axs[1][0].set_xlabel('Duration (seconds)')
+axs[1][0].set_ylabel('Popularity')
+
+axs[1][1].scatter(spotify_df['year'], spotify_df['popularity'], color='#cc4365', s=5)
+axs[1][1].set_xlabel('Year')
+axs[1][1].set_ylabel('Popularity')
 
 plt.show()
